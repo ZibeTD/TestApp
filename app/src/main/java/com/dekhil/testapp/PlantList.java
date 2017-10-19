2209fb195f8a72cde31aaa3d673417ee2700e0eb
ZibeTD <zibet2@dekhil.ru>
2017-10-19 23:56:41 +0300

    Plant spinner added

--- app/src/main/java/com/dekhil/testapp/PlantList.java
+++ app/src/main/java/com/dekhil/testapp/PlantList.java
@@ -11,12 +11,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link TUList2.OnFragmentInteractionListener} interface
+ * {@link PlantList.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link TUList2#newInstance} factory method to
+ * Use the {@link PlantList#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TUList2 extends Fragment {
+public class PlantList extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,7 +28,7 @@ public class TUList2 extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public TUList2() {
+    public PlantList() {
         // Required empty public constructor
     }
 
@@ -38,11 +38,11 @@ public class TUList2 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TUList2.
+     * @return A new instance of fragment PlantList.
      */
     // TODO: Rename and change types and number of parameters
-    public static TUList2 newInstance(String param1, String param2) {
-        TUList2 fragment = new TUList2();
+    public static PlantList newInstance(String param1, String param2) {
+        PlantList fragment = new PlantList();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,7 +63,7 @@ public class TUList2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tulist, container, false);
+        return inflater.inflate(R.layout.fragment_plantlist, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/dekhil/testapp/ReportConsumerProperties.java
+++ app/src/main/java/com/dekhil/testapp/ReportConsumerProperties.java
@@ -3,16 +3,30 @@ package com.dekhil.testapp;
         import android.net.Uri;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.Spinner;
         import android.widget.TextView;
 
         import com.dekhil.testapp.dummy.DummyContent;
 
-public class ReportConsumerProperties extends AppCompatActivity implements TUFragment.OnTUListFragmentInteractionListener, PlantFragment.OnPlantListFragmentInteractionListener, TUList2.OnFragmentInteractionListener {
+public class ReportConsumerProperties extends AppCompatActivity implements TUFragment.OnTUListFragmentInteractionListener, PlantFragment.OnPlantListFragmentInteractionListener, TUList.OnFragmentInteractionListener, PlantList.OnFragmentInteractionListener, AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report__consumer_properties);
+
+        Spinner spinner = (Spinner) findViewById(R.id.tu_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
 
@@ -30,6 +44,23 @@ public class ReportConsumerProperties extends AppCompatActivity implements TUFra
 
     @Override
     public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (parent.getId() == R.id.tu_spinner) {
+            TextView text = (TextView) findViewById(R.id.tu_list_item_selected);
+            text.setText((String) parent.getItemAtPosition(position));
+        }
+        else {
+            TextView text = (TextView)findViewById(R.id.plant_list_item_selected);
+            text.setText((String) parent.getItemAtPosition(position));
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
